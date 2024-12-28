6001bff534ea84ed51b6ea49687fc6cebe2ee2aa
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-28 16:11:52 +0000

    update

--- scripts/soft_smoe_mamba.sh
+++ scripts/soft_smoe_mamba.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/softmoe/result/soft_mamba.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-017
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
 #SBATCH --partition=research
